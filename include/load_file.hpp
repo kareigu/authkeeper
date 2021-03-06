e34e17c8e80020ab84e4efc008950678a5fbe4ef
mxr <eetu.kivelae@gmail.com>
2021-03-06 18:41:40 +0200

    Moved function definition

--- include/load_file.hpp
+++ include/load_file.hpp
@@ -2,17 +2,4 @@
 #include <string>
 #include <fstream>
 
-std::string* load_keylist() {
-  std::ifstream inputStream("keys.txt");
-  std::string* output = new std::string[300];
-
-  if(inputStream.is_open()) {
-    for(int i = 0; i < 300; i++) {
-      std::getline(inputStream, output[i]);
-    }
-
-    inputStream.close();
-  }
-
-  return output;
-}
+std::string* load_keylist();
---
+++ src/load_file.cpp
@@ -0,0 +1,16 @@
+#include "load_file.hpp"
+
+std::string* load_keylist() {
+  std::ifstream inputStream("keys.txt");
+  std::string* output = new std::string[300];
+
+  if(inputStream.is_open()) {
+    for(int i = 0; i < 300; i++) {
+      std::getline(inputStream, output[i]);
+    }
+
+    inputStream.close();
+  }
+
+  return output;
+}
