6dea314196ee48673aa1fdfe2972cdc55ab69ccd
mxr <eetu.kivelae@gmail.com>
2021-03-07 17:18:17 +0200

    Fixed indentation to match other files

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,25 @@
 #include "config.hpp"
 
 int main() {
+  clear();
+  print_header();
+
+  Config* config = new Config();
+
+  Keylist* keylist = new Keylist();
+
+  CommandList* command_list = new CommandList(keylist, config);
+  
+  CommandParser* command_parser = new CommandParser(command_list);
+
+  while (true) {
+    std::string input;
+    std::cout << config->symbol << " ";
+    std::cin >> input;
     clear();
     print_header();
-
-    Config* config = new Config();
-
-    Keylist* keylist = new Keylist();
-
-    CommandList* command_list = new CommandList(keylist, config);
-    
-    CommandParser* command_parser = new CommandParser(command_list);
-
-    while (true) {
-        std::string input;
-        std::cout << config->symbol << " ";
-        std::cin >> input;
-        clear();
-        print_header();
-        command_parser->parse_command(input);
-    }
-    
-    return 0;
+    command_parser->parse_command(input);
+  }
+  
+  return 0;
 }
